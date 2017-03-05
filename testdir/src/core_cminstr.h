40a99497421a5d1a0e1f516b57018288a442f4e3
ccma <ccma@nuvoton.com>
2017-03-05 14:51:49 +0800

    COMMIT_EDITMSG

--- testdir/src/core_cminstr.h
+++ testdir/src/core_cminstr.h
@@ -5,7 +5,7 @@
  * @date     20. October 2015
  ******************************************************************************/
 /* Copyright (c) 2009 - 2015 ARM LIMITED
-
+ 
    All rights reserved.
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
